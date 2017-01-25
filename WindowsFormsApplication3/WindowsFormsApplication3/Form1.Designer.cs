2a1c66891fd739ee5e5dfdbb8f0faaecc64bb8cc
admiralnelson <admiralofinternetmeme@outlook.com>
2017-01-25 19:43:37 +0800

    1st upload

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.kerja = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(12, 12);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(186, 20);
+            this.txt_search.TabIndex = 1;
+            // 
+            // pic
+            // 
+            this.pic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pic.Location = new System.Drawing.Point(0, 0);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(606, 433);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // kerja
+            // 
+            this.kerja.DoWork += new System.ComponentModel.DoWorkEventHandler(this.kerja_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 433);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.PictureBox pic;
+        private System.ComponentModel.BackgroundWorker kerja;
+    }
+}
+
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+
+        //kamus global
+        string data;
+        string path = AppDomain.CurrentDomain.BaseDirectory;
+        //end kamus global
+
+        public class imdb
+        {
+            public string Title { get; set; }
+            public string Year { get; set; }
+            public string Rated { get; set; }
+            public string Released { get; set; }
+            public string Runtime { get; set; }
+            public string Genre { get; set; }
+            public string Director { get; set; }
+            public string Writer { get; set; }
+            public string Actors { get; set; }
+            public string Plot { get; set; }
+            public string Language { get; set; }
+            public string Country { get; set; }
+            public string Awards { get; set; }
+            public string Poster { get; set; }
+            public string Metascore { get; set; }
+            public string imdbRating { get; set; }
+            public string imdbVotes { get; set; }
+            public string imdbID { get; set; }
+            public string Type { get; set; }
+            public string Response { get; set; }
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void dl_hmtl(string url, ref string output)
+        {
+            WebClient client = new WebClient();
+            button1.Invoke(new MethodInvoker(delegate 
+                {
+                    button1.Text = "tunggu";
+                    button1.Enabled = false;
+                }));
+            output = client.DownloadString(url);
+            imdb isi_data = JsonConvert.DeserializeObject<imdb>(data);
+            MessageBox.Show(data);
+            MessageBox.Show(isi_data.Title);
+            MessageBox.Show(isi_data.Year);
+            MessageBox.Show(isi_data.Plot);
+            MessageBox.Show(isi_data.Poster);
+            if (isi_data.Poster != null)
+            { 
+                client.DownloadFile(isi_data.Poster, path + "thumb");
+               
+                //output =  "test";
+                pic.ImageLocation = path + "thumb";
+                pic.Load();
+            }
+            button1.Invoke(new MethodInvoker(delegate 
+            {
+                button1.Text = "ok";
+                button1.Enabled = true;
+            }));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string raw_text, search_term="";
+            
+            raw_text = this.txt_search.Text;
+            for (int i=0; i < raw_text.Length; i++)
+            {
+                if (raw_text[i] !=' ')
+                {
+                    search_term = search_term + raw_text[i]; 
+                }
+                else
+                {
+                    search_term = search_term + '+';
+                }
+            }
+
+
+            MessageBox.Show(path);
+            MessageBox.Show(search_term);
+            kerja.RunWorkerAsync(search_term);
+           
+        }
+
+        private void kerja_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //MessageBox.Show(e.Argument.ToString);
+            dl_hmtl("http://www.omdbapi.com/?t=" + e.Argument + "&y=&plot=short&r=json", ref data);
+        }
+
+       
+    }
+}
